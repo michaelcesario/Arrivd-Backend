601aeb70e815436f6b06d0b98a96b9d645a891d2
michaelcesario <michael_c_4@live.ca>
2017-11-14 21:37:23 -0500

    fixed login bug

--- Resources/User.py
+++ Resources/User.py
@@ -10,14 +10,21 @@ class User(Resource):
         dbConnection = DatabaseConnection.getDBCursor()
         cursor = dbConnection.cursor()
 
-        query = "select * from users where username = %s"
-        cursor.execute(query, (username,))
-        result = cursor.fetchone()
+        queryUsername = "select * from users where username = %s"
+        cursor.execute(queryUsername, (username,))
+        result1 = cursor.fetchone()
+
+        queryNumber = "select * from users where phonenumber = %s"
+        cursor.execute(queryNumber, (username,))
+        result2 = cursor.fetchone()
+
         dbConnection.close()
 
-        print(result)
-        user = UserModel(result[0], result[1], result[2], result[3])
-        if result:
+        if result1:
+            user = UserModel(result1[0], result1[1], result1[2], result1[3])
+            return user.json()
+        elif result2:
+            user = UserModel(result2[0], result2[1], result2[2], result2[3])
             return user.json()
         else:
             return {"message": "user not found"}
